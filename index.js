03bbf7cc83d31cd0b2a78f2058695a15304d3b99
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2024-06-28 20:14:18 +0200

    timeouts

--- index.js
+++ index.js
@@ -41,6 +41,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use('/user',user);
 
 // Start Server
-app.listen(PORT,'0.0.0.0', () => {
+const server = app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+server.keepAliveTimeout = 120 * 1000; // 120 segundos
+server.headersTimeout = 120 * 1000; // 120 segundos
